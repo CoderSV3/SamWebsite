9efa8d139d7008c7077a4d0502d2279a2de066a3
CoderSV3 <samuelviveros3@gmail.com>
2020-09-27 20:20:11 -0400

    Briefly modified the JavaScript in script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert('Note: This page is under maintenance.');
+var maintenanceCount = 0;
+var doc = document.getElementById("index.html");
+//if (doc.includes("(Under Maintenance)") == true) {
+	//maintenanceCount++;
+//}
+alert('Note: This page is under maintenance. Pages currently under maintenance: ' + ++maintenanceCount);
+// This page is temporarily hardcoded. maintenanceCount will be properly updated later.
